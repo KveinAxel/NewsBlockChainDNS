34e01c765a81f8ecf3d065804743ab7eb8f13507
Kevin Axel <2438679008@qq.com>
2020-04-12 05:17:48 +0800

    将注释更新为中文

--- main.py
+++ main.py
@@ -4,41 +4,56 @@ import re
 app = FastAPI()
 superNodes = dict()
 
-# ip reg
+# IP的正则表达式
 pattern = re.compile(r'((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}')
 
 
+# 请求成为普通节点
 @app.post("/node")
 async def node(ip: str):
-    # check ip
+
+    # 检查IP是否合法
     if pattern.search(ip):
+
+        # 将超级节点排序
         res = sorted(superNodes.items(), key=lambda x: x[1])
         if len(res):
+
+            # 取出负载最少的节点
             res = res[0][0]
+
+            # 负载数加一
             superNodes[res] = superNodes[res] + 1
-            # request superNode to save ip
+
+            # TODO 请求超级节点以保存IP
+            # 返回超级节点的IP
             return {"ip": res}
         else:
-            return {"error": "there is no super node left"}
+            return {"error": "无超级节点，无法加入区块网络"}
     else:
-        return {"error": "the ip format is wrong"}
+        return {"error": "IP地址格式异常"}
 
 
+# 请求成为超级节点
 @app.post("/superNode")
-async def superNode(ip: str):
-    # check ip
+async def super_node(ip: str):
+
+    # 检查IP地址
     if pattern.search(ip):
+
+        # 加入节点，负载为0
         if ip not in superNodes:
             superNodes[ip] = 0
-            return {"success": "inserted successfully"}
+            return {"success": "加入区块网络成功"}
         else:
-            return {"error": "the ip has been super node, please don't do it again"}
+            return {"error": "该IP已经成为超级节点，请勿重复添加"}
     else:
-        return {"error": "the ip format is wrong"}
+        return {"error": "IP地址格式异常"}
 
 
+# 向超级节点广播区块
 @app.post("/broadcast")
 async def broadcast(block: str):
     for key in superNodes.keys():
-        # request the block to superNodes
+        # 向超级节点广播区块
         pass
